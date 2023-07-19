e76e40eb67c89be56d2bae4a4e48840ce228434b
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-19 19:05:56 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,42 +1,50 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - insertion sort algorithm
+ * cocktail_sort_list - insertion sort algorithm
  * @list: list
  */
-void insertion_sort_list(listint_t **list)
+void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node, *tmp, *ins;
+	listint_t *node, *next_node, *prev_node;
+	int swpd;
 
 	if (!list || !(*list) || !(*list)->next)
 		return;
-	ins = (*list)->next;
-	while (ins)
-	{
-		node = ins;
-		for (tmp = node->prev; tmp; tmp = tmp->prev)
+	node = (*list);
+	do {
+		swpd = 0;
+		for (; node->next; node = node->next)
 		{
-			if (tmp->n > node->n)
+			if (node->n > node->next->n)
 			{
-				node->prev->next = node->next;
-				if (node->next)
-					node->next->prev = node->prev;
-				node->next = tmp;
-				if (tmp->prev)
-				{
-					tmp->prev->next = node;
-					node->prev = tmp->prev;
-				}
-				else
-				{
-					*list = node;
-					node->prev = NULL;
-				}
-				tmp->prev = node;
+				swpd = 1;
+				next_node = node->next;
+				node->next = next_node->next;
+				node->prev = next_node;
+				next_node->prev = node->prev;
+				next_node->next = node;
+				if (!next_node->prev)
+					*list = next_node;
 				print_list(*list);
-				tmp = node;
 			}
 		}
-		ins = ins->next;
-	}
+		if (!swpd)
+			break;
+		for (; node->prev; node = node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				swpd = 1;
+				prev_node = node->prev;
+				prev_node->next = node->next;
+				prev_node->prev = node;
+				node->prev = prev_node->prev;
+				node->next = prev_node;
+				if (!node->prev)
+					*list = node;
+				print_list(*list);
+			}
+		}
+	} while (swpd);
 }
