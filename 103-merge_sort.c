f96633d4cba0602fb6ee80fff6439ea093f739d0
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-20 17:56:31 +0100

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -61,7 +61,7 @@ void TopDownSplitMerge(int *b, int *a, size_t i, size_t j)
 
 	if (j - i <= 1)
 		return;
-	k = (j + i) / 2;
+	k = (size_t)((j + i) / 2);
 	TopDownSplitMerge(a, b, i, k);
 	TopDownSplitMerge(a, b, k, j);
 	TopDownMerge(b, a, i, k, j);
