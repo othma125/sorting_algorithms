4158c856685bd6f93e6f8f29fd14de9c56d08e20
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-21 19:44:35 +0100

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -56,7 +56,7 @@ void sort(int *array, size_t size, int x, int y)
 	if (x >= 0 && y >= 0 && x < y)
 	{
 		p = partition(array, size, x, y);
-		sort(array, size, x, p);
+		sort(array, size, x, p - 1);
 		sort(array, size, p, y);
 	}
 }
