be78b27fe0bc0c22caf3a11b3fb40b89f5ab11d1
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-22 23:30:42 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -5,11 +5,8 @@
  * @c2: second card.
  * Return: difference in value.
  */
-int cmpfunc(const void *c1, const void *c2)
+int cmpfunc(const card_t *card1, const card_t *card2)
 {
-	card_t *card1 = (card_t *)c1;
-	card_t *card2 = (card_t *)c2;
-
 	if (strcmp(card1->value, card2->value) == 0)
 		return (card1->kind - card2->kind);
 	return (strcmp(card1->value, card2->value));
