e72e9555f612e0f5265d7654a1ca42246888f9a1
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-22 23:22:14 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -14,10 +14,67 @@ int cmpfunc(const void * c1, const void * c2)
 		return (card1->kind - card2->kind);
 	return (strcmp(card1->value, card2->value));
 }
+
+/**
+ * swap_list - swap
+ * @list: list
+ * @node1: list
+ * @node2: list
+ * Return: nothing
+ */
+void swap_list(deck_node_t **list, deck_node_t *node1, deck_node_t *node2)
+{
+	node1->next = node2->next;
+	if (node2->next)
+		node2->next->prev = node1;
+	node2->prev = node1->prev;
+	if (node1->prev)
+		node1->prev->next = node2;
+	node1->prev = node2, node2->next = node1;
+	if (!node2->prev)
+		*list = node2;
+}
+/**
+ * cocktail_sort_list - insertion sort algorithm
+ * @list: list
+ * Return: nothing
+ */
+void cocktail_sort_list(deck_node_t **list)
+{
+	deck_node_t *node;
+	int swpd;
+	node = *list;
+	do {
+		swpd = 0;
+		for (; node->next; node = node->next)
+		{
+			if (cmpfunc(node, node->next) < 0)
+			{
+				swpd = 1;
+				swap_list(list, node, node->next);
+				node = node->next;
+			}
+		}
+		if (!swpd)
+			break;
+		for (; node->prev; node = node->prev)
+		{
+			if (cmpfunc(node, node->prev) < 0)
+			{
+				swpd = 1;
+				swap_list(list, node->prev, node);
+				node = node->prev;
+			}
+		}
+	} while (swpd);
+}
 /**
  * sort_deck - sort_deck algorithm
  * @list: list
  */
 void sort_deck(deck_node_t **deck)
 {
+	if (!deck || !(*deck) || !(deck->next))
+		return;
+	cocktail_sort_list(deck);
 }
