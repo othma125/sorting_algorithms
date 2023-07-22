4ba9db2e2878745f98c731a723cec5e3a81c4347
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-22 21:51:04 +0100

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -36,9 +36,9 @@ int partition(int *array, size_t size, int x, int y)
 		} while (array[j] > pivot);
 		if (i > j)
 			return (j);
-		if (array[i] > array[j] && i != j)
+		if (array[i] > pivot)
 		{
-			swap(array + i, array + j);
+			swap(array + i, array + index);
 			print_array(array, size);
 		}
 	}
