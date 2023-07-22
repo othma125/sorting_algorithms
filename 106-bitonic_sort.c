eaf38a7715d504ab9b099a1e9552254ed9c62f1e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-22 15:51:32 +0100

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,10 +1,31 @@
 #include "sort.h"
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int aux = *a;
 
+	*a = *b;
+	*b = aux;
+}
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void compAndSwap(int a[], int i, int j, int direction) 
 { 
   if (direction == (a[i] > a[j])) 
-    swap(a[i], a[j]); 
+    swap(a + i, a + j); 
 } 
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void bitonicMerge(int a[], int x, int y, int direction) 
 {
 int k = y/2;
@@ -16,7 +37,12 @@ compAndSwap(a, i, i + k, direction);
 bitonicMerge(a, x, k, direction); 
 bitonicMerge(a, x + k, k, direction); 
 } 
-} 
+}
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void sort(int a[],int x, int y, int direction) 
 {
   int k;
@@ -27,7 +53,12 @@ bitonicSort(a, x, k, 1);
 bitonicSort(a, x + k, k, 0); 
 bitonicMerge(a, x, y, direction); 
 	} 
-} 
+}
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void bitonic_sort(int a[], size_t size) 
 { 
 sort(a, 0, size, 1); 
